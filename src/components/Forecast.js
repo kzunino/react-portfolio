3f6e4f8b06e66662d19da98c71d89f3dca4a61be
Kyle <kzunino@gmail.com>
2020-07-22 18:22:42 -0700

    project page has web stack, problem, thoughts, more images

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import weatherForecast from './img/fullWeather.png';
+import weatherForecast2 from './img/fullWeather2.png';
 import reactLogo from './img/react-icon.png';
 import herokuLogo from './img/heroku-icon.png';
 
@@ -7,16 +8,16 @@ export default class Project extends Component {
   render() {
     return (
       <div className='container-fluid p-2'>
-        <main className='container-fluid mt-5 px-sm-0'>
+        <main className='container-fluid mt-5 px-0'>
           <h1 className='font-weight-bold mt-5'>Weather Forecast</h1>
-          <p className='col-md-9 pl-0 text-secondary'>
-            Weather Forecast is a simple application I put together after
-            learning the basics of Bootstrap 4. I wanted a way to practice
-            styling, while at the same time exercise my skills at managing large
-            data sets from OpenWeatherMap API.
+          <p className='col-md-9 p-0 text-secondary'>
+            Weather Forecast is a simple application I created after learning
+            the basics of Bootstrap 4. I wanted a way to practice styling, while
+            at the same time exercise my skills at managing large data sets from
+            OpenWeatherMap API.
           </p>
         </main>
-        <section className='container-fluid mt-5 px-sm-0'>
+        <section className='container-fluid mt-5 px-0'>
           <table className='table table-borderless table-sm'>
             <thead>
               <tr>
@@ -40,7 +41,7 @@ export default class Project extends Component {
                 <td className='font-weight-light'>React</td>
                 <td className='secondary-color font-weight-bold'>
                   <a
-                    href='https://github.com/kzunino'
+                    href='https://github.com/kzunino/react-weather-app'
                     target='_blank'
                     rel='noopener noreferrer'
                   >
@@ -72,10 +73,10 @@ export default class Project extends Component {
           alt='forecast weather app'
           className='project-main-photo'
         />
-        <section className='container-fluid mt-5 px-sm-0'>
+        <section className='container-fluid mt-5 px-0'>
           <h2 className='mt-5'>Web Stack</h2>
-          <div className='row container-fluid pr-0 align-items-center'>
-            <p className='col-md-7 pl-0 text-secondary'>
+          <div className='row container-fluid pr-0 mr-sm-0 align-items-center'>
+            <p className='col-md-7 p-0 text-secondary'>
               For this project I decided to use React and Heroku. I wanted to
               make a very simple and clean weather application in order to
               practice state management, working with larger data sets, and
@@ -98,17 +99,74 @@ export default class Project extends Component {
               </div>
               <br></br>
               <br></br>
+              <br></br>
               <div className='program-icon d-inline heroku'>
                 <img
                   src={herokuLogo}
                   alt='Heroku Logo'
-                  className='program-icon-img '
+                  className='program-icon-img heroku'
                 />{' '}
                 Heroku
               </div>
             </div>
           </div>
         </section>
+
+        <section className='container-fluid mt-5 px-0'>
+          <h2 className='mt-5 text-center'>Problems &amp; Afterthoughts</h2>
+          <div className='row justify-content-center'>
+            <p className='col-md-9 text-center text-secondary'>
+              One of the issues I had with this project was getting accurate
+              icons to render for the correct weather patterns. The API doesn't
+              use a numerical database to reference different weather patterns
+              so I was forced to use their general descriptions such as "clear"
+              or "rain" in order to render an icon. Because of this, there isn't
+              great diversity in the icons used for different weather patterns.
+              <br></br>
+              <br></br>
+              One way to improve this project would be to save the zip code in
+              local storage or as a cookie so the user doesn't have to re-enter
+              their zip code every time the page refreshes or loads. It's a
+              slight hassle to constantly have to input a your zip code.
+              Additionally, it would be awesome if this project could be global,
+              however, it only accepts valid US zip codes.
+              <br></br>
+              <br></br>
+              Another improvement would be to make it more responsive. As it is
+              now, the application is nowhere near ready for viewing on a mobile
+              device. I didn't really take this into account when I started
+              because I wanted to make a widget-like application, but I think it
+              could be made into a smaller, clean mobile version as well.
+            </p>
+          </div>
+        </section>
+        <img
+          src={weatherForecast2}
+          alt='forecast weather app'
+          className='project-main-photo'
+        />
+        <div className='row justify-content-center'>
+          <div className='col-12 col-md-2 text-center'>
+            <a
+              className='link-decoration '
+              href='https://github.com/kzunino/react-weather-app'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              View Code »
+            </a>
+          </div>
+          <div className='col-12 col-md-2 text-center'>
+            <a
+              className='link-decoration'
+              href='https://kz-react-weather-app.herokuapp.com/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              View Live »
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
